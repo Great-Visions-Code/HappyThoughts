46f6af76a8606a582b2134118876e776c8a94425
Gustavo Vazquez <160083465+Great-Visions-Code@users.noreply.github.com>
2024-04-10 22:51:55 -0700

    Update WelcomeView.swift

--- Views/WelcomeView.swift
+++ Views/WelcomeView.swift
@@ -1,5 +1,5 @@
 //
-//  SwiftUIView.swift
+//  WelcomeView.swift
 //  HappyThoughts
 //
 //  Created by Gustavo Vazquez on 4/7/24.
@@ -12,22 +12,34 @@ struct WelcomeView: View {
         ZStack {
             AppColors.mainBackgroundColor
                 .ignoresSafeArea()
-            VStack() {
+            
+            VStack(alignment: .center) {
                 Image("WelcomeViewImage")
                     .resizable()
-                    .scaledToFill()
-        
+                    .aspectRatio(contentMode: .fill)
+                    .shadow(color: .blue.opacity(0.6),
+                            radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+                    .clipped(antialiased: true)
+                    .padding(EdgeInsets(top: 0, 
+                                        leading: 0,
+                                        bottom: 15,
+                                        trailing: 0))
                 Text("HappyThoughts")
-                    .font(.largeTitle)
+                    .font(.system(.title,
+                                  design: .serif))
+                    .bold()
+                    .padding()
                 Text("''Gratefulness Quote Holder''")
+                    .font(.system(.headline, 
+                                  design: .serif))
                     .padding()
                 Buttons.GeneralButtons(text: "Login",
                                        action: {
-                    //TODO: Link to HomeView()
+                    // TODO: Link to HomeView()
                 })
-                Buttons.GeneralButtons(text: "Sign Up", 
+                Buttons.GeneralButtons(text: "Sign Up",
                                        action: {
-                    //TODO: Link to SignUpView()
+                    // TODO: Link to SignUpView()
                 })
             }
         }
